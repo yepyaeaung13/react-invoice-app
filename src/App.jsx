958102b4d7333c3ab5d6db39be064017dc72bc5e
Ye Pyae Aung <yepyaeaung1311999@gmail.com>
2024-05-19 12:25:53 +0630

    change bg color

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ const App = () => {
   };
 
   return (
-    <main className="flex flex-col min-h-screen">
+    <main className="flex flex-col min-h-screen bg-white">
       <div>
         <Toaster />
       </div>
